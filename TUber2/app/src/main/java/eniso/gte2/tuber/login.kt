88548b4f1bf61f5773b7b72c85e2ef58be01a373
adam <adam.haddad.85845@gmail.com>
2022-12-05 18:13:38 +0100

    login2

---
+++ TUber2/app/src/main/java/eniso/gte2/tuber/login.kt
@@ -0,0 +1,23 @@
+package eniso.gte2.tuber
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eniso.gte2.tuber.databinding.ActivityLoginBinding
+import eniso.gte2.tuber.databinding.ActivityMainBinding
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    binding.button9.setOnClickListener {
+        val i = Intent(this,homee::class.java)
+        startActivity(i)
+    }
+        binding.button8.setOnClickListener {
+            val i = Intent(this,Number_Request::class.java)
+            startActivity(i)
+        }
+    }
+}
