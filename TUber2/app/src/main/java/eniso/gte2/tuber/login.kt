25dd56ffb3686ae57a290b88b243f0b9d31467a0
adam <adam.haddad.85845@gmail.com>
2022-12-11 14:53:02 +0100

    no firebase

--- TUber2/app/src/main/java/eniso/gte2/tuber/login.kt
+++ TUber2/app/src/main/java/eniso/gte2/tuber/login.kt
@@ -1,5 +1,6 @@
 package eniso.gte2.tuber
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,8 @@ import eniso.gte2.tuber.databinding.ActivityLoginBinding
 import eniso.gte2.tuber.databinding.ActivityMainBinding
 
 class login : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -14,6 +17,7 @@ class login : AppCompatActivity() {
     binding.button9.setOnClickListener {
         val i = Intent(this,homee::class.java)
         startActivity(i)
+        //launchSignInFlow()
     }
         binding.button8.setOnClickListener {
             val i = Intent(this,Number_Request::class.java)
